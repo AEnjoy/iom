e845ceda1f0a4970381ad464be763654cdf7dd42
AEnjoy <2328790874@qq.com>
2024-02-11 17:48:43 +0800

    Upgrade build.sh

--- server/build.sh
+++ server/build.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
-
+# windows/amd64,386,amr64,arm32
+# linux/amd64,386,486,arm32
+#
+#
+#
+#
+#
 Version=$(cat version)
 COMMIT=$(git rev-parse --short HEAD)
 GoVersion=$(echo $(go version) | awk '{print $3}')
 Time=$(date "+%Y-%m-%d:::%H:%M:%S")
-export GOOS=windows
-export GOARCH=amd64
+export GOOS=linux
+export GOARCH=386
+export CGO_LDFLAGS="-static"
 
-go build -ldflags "-X main.Version=$Version -X main.GitHash=$COMMIT -X main.GoVersion=$GoVersion -X main.BuildTime=$Time"  ./main.go
+go build  -v -ldflags "-s -w -X main.Version=$Version -X main.GitHash=$COMMIT -X main.GoVersion=$GoVersion -X main.BuildTime=$Time"  ./main.go
